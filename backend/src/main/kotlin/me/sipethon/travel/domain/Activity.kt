0e9472030a8d639dc787808f1e4380939a00224e
kshired <shiroed1211@gmail.com>
2024-05-25 02:11:08 +0900

    fix : invalid spec

--- backend/src/main/kotlin/me/sipethon/travel/domain/Activity.kt
+++ backend/src/main/kotlin/me/sipethon/travel/domain/Activity.kt
@@ -2,7 +2,7 @@ package me.sipethon.travel.domain
 
 data class Activity(
     val title: String,
-    val category: String,
+    val category: String?,
     val type: ActivityType,
     val imgUrl: String?
 )
