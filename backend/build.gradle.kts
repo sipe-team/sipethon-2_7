dd3bbe9684f15049a01d109bbef1b8ee4228d6b0
Kijun Kwon <39583312+kkjsw17@users.noreply.github.com>
2024-05-25 02:01:41 +0900

    [BE] prod 프로필 추가 (#15)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,7 +24,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	implementation("com.h2database:h2")
+	implementation("com.mysql:mysql-connector-j")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
